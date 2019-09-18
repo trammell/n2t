5af8d1dc5552c2537fcfe28197176b0cdff3f67f
John Trammell <john.trammell@outsell.com>
2019-09-17 20:48:40 -0500

    added comments with ideas

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,49 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+    // initialize screen to white
+    // loop forever over screen addresses?
+    // in loop, if any key is pressed put black pixel
+    // otherwise put white pixel
+
+
+    // need to use array syntax
+
+
+    @R2
+    M=0
+
+
+    @SCREEN
+    D=A
+    @R0
+    M=0
+
+(LOOP)
+
+    // jump to end if R0 is at the end of the screen (@SCREEN + 8192)
+    @R0
+    D=M
+    @END
+    D;JEQ
+
+    // R2 = R2 + R1
+    @R1
+    D=M
+    @R2
+    M=D+M
+
+    // R0 = R0 - 1
+    @R0
+    M=M-1
+
+    // jump to top of loop
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
+
+
+
