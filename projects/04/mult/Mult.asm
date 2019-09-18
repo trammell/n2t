0f2b86951b353caf7089ab6cef88a05ed7e3d9e1
John Trammell <john.trammell@outsell.com>
2019-09-17 20:34:42 -0500

    added initialize R2=0

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -6,6 +6,10 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
+    // initialize R2 = 0
+    @R2
+    M=0
+
 (LOOP)
 
     // jump to end if R0 = 0
