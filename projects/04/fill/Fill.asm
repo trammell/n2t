7e17d0102b9fb516cdb6b7962acec8da8c0cc967
John Trammell <john.trammell@outsell.com>
2019-09-18 22:42:01 -0500

    sketching in asm functionality I want

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,3 +1,4 @@
+// vim: set ai et ts=4 :
 // This file is part of www.nand2tetris.org
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
@@ -11,49 +12,90 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-    // initialize screen to white
-    // loop forever over screen addresses?
-    // in loop, if any key is pressed put black pixel
-    // otherwise put white pixel
+// the screen has 256 rows and 512 columns.
+// The address of the first 16-bit word is @SCREEN (16384)
+// Each row has 32 16-bit words.
+// There are 8192 words in total for the screen.
+
+// initialize screen to white
+// loop forever over screen addresses?
+// in loop, if any key is pressed put black pixel
+// otherwise put white pixel
+// need to use array syntax?
 
 
-    // need to use array syntax
+// set symbol @white to 0
+@white
+M=0
+
+// set symbol @black to 65535 (0xffff)
+@32767
+D=A
+D=D+A
+D=D+1
+@black
+M=D
+
+// set symbol @word to 0
+@word
+M=0
+
+// increment @word
+@word
+M=M+1
+
+// set @SCREEN[@word] = @color
+@SCREEN
+M=D
+
+@SCREEN
+A=A+1
+M=D
 
 
-    @R2
-    M=0
+// KBD is 0 when no key is pressed
+// if KBD is 0, then set R0 to R1
+//     else set R0 to R2
+
+@KBD
+
+//D=D+1
+//A=D
+//M=1
 
 
-    @SCREEN
-    D=A
-    @R0
-    M=0
-
-(LOOP)
-
-    // jump to end if R0 is at the end of the screen (@SCREEN + 8192)
-    @R0
-    D=M
-    @END
-    D;JEQ
-
-    // R2 = R2 + R1
-    @R1
-    D=M
-    @R2
-    M=D+M
-
-    // R0 = R0 - 1
-    @R0
-    M=M-1
-
-    // jump to top of loop
-    @LOOP
-    0;JMP
+//    @R2
+//    M=0
+//
+//
+//    @SCREEN
+//    D=A
+//    @R0
+//    M=0
+//
+//(LOOP)
+//
+//    // jump to end if R0 is at the end of the screen (@SCREEN + 8192)
+//    @R0
+//    D=M
+//    @END
+//    D;JEQ
+//
+//    // R2 = R2 + R1
+//    @R1
+//    D=M
+//    @R2
+//    M=D+M
+//
+//    // R0 = R0 - 1
+//    @R0
+//    M=M-1
+//
+//    // jump to top of loop
+//    @LOOP
+//    0;JMP
+//
 
 (END)
     @END
     0;JMP
-
-
-
