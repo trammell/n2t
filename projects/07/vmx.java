fcbec2983f75c9b97b3a09966ea3aa03cc33f97d
John Trammell <john.trammell@gmail.com>
2020-05-25 19:41:28 -0500

    hw

--- projects/07/vmx.java
+++ projects/07/vmx.java
@@ -1,5 +1,16 @@
+/**
+ * VMX - Virtual Machine Xlator for nand2tetris
+ * 
+ * See textbook chapters 7 and 8 for details.
+ */
+public class VMX {
 
 
+public static void main(String[] args) {
+    System.out.println("Hello VMX World");
+
+}
+}
 
 
 
