eeb64c1df3a455ffd463b6e3740ee637559dfa3d
John Trammell <john.trammell@gmail.com>
2020-07-05 19:48:29 -0500

    converting to kotlin

---
+++ projects/07/main.kt
@@ -0,0 +1,8 @@
+/**
+ * VMX - Virtual Machine Xlator for nand2tetris
+ * 
+ * See textbook chapters 7 and 8 for details.
+ */
+fun main (args: Array<String>)  {
+  println("Hello VMTranslator World"); 
+}
