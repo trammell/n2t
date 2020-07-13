a49a3a80dd67a82b819f4fbf13b947fae591b39e
John Trammell <john.trammell@gmail.com>
2020-07-12 22:46:01 -0500

    handle directories properly

--- projects/07/vmtrans.kt
+++ projects/07/vmtrans.kt
@@ -4,43 +4,57 @@
  * See textbook chapters 7 and 8 for details.
  */
 
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
-
-fun main(args: Array<String>) {
-  println("Hello VMTranslator World " + args[0])
-  if (args.isEmpty()) {
-    print("Please add some command line arguments")
-    return
-  }
-  
-  // we only create one CodeWriter object
-  val cw = CodeWriter(System.out.writer())
-
-  // create a parser for each argument
-  for (arg in args) {
-    val parser = Parser(arg)
-    cw.writeComment("starting parse of file: ${arg}")
-
-    cw.writeComment()
-  }
-// print("Hello ${args[0]}")
-  // if it's a file, read from the file
-  // if it's a directory, run on all files within
-}
+import java.io.File
 
 enum class CType {
   C_NONE, C_ARITHMETIC, C_PUSH, C_POP, C_LABEL, C_GOTO,
   C_IF, C_FUNCTION, C_RETURN, C_CALL
 }
 
-class Parser(val filename: String) {
+fun main(args: Array<String>) {
 
-  fun hasMoreCommands() {
+  if (args.isEmpty()) {
+    println("usage: vmtrans [<file>.vm | directory/]")
+    return
   }
 
-  fun advance() {
+  // we only create one CodeWriter object
+  val cw = CodeWriter()
+
+  // create a parser for each argument, handling directories correctly
+  for (path in args) {
+    var file = File(path);
+
+    if (file.isDirectory()) {
+      file
+        .walk()
+        .filter { it.isFile }
+        .filter { it.extension == "vm" }
+        .forEach { parseFile(it.canonicalPath, cw) }
+    } else {
+      parseFile(path, cw)
+    }
   }
+
+}
+
+fun parseFile(path: String, cw: CodeWriter) {
+  val parser = Parser(path)
+  cw.setFileName(path)
+  File(path).forEachLine {
+    parser.command = it
+    when (parser.commandType()) {
+      C_PUSH -> cw.writePushPop( C_PUSH, parser.arg1())
+      else -> {
+        cw.writeComment(it)
+      }
+    }
+  }
+}
+
+class Parser {
+
+  command: String = ""
 
   fun commandType() {
   }
@@ -52,18 +66,16 @@ class Parser(val filename: String) {
   }
 }
 
-class CodeWriter(val ostream: OutputStreamWriter) {
+class CodeWriter() {
 
-  fun setFileName() {
+  fun setFileName(name: String) {
+    writeComment("Parsing file: ${name}")
   }
 
-  fun writeComment() {
-    println()
+  fun writeComment(comment: String = "") {
+    println(if (comment.isBlank()) "" else "// ${comment}")
   }
 
-  fun writeComment(comment: String) {
-    println("// " + comment)
-  }
 
   fun writeArithmetic() {
   }
@@ -72,6 +84,5 @@ class CodeWriter(val ostream: OutputStreamWriter) {
   }
 
   fun close() {
-
   }
 }
