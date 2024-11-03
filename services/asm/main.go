da0a0065bd3b3690c14963ba48ec76c9ba56400f
John Trammell <john.trammell@gmail.com>
2024-11-02 19:44:24 -0500

    cleaning up main():wq

--- services/asm/main.go
+++ services/asm/main.go
@@ -8,6 +8,12 @@ import (
 // main function: takes no arguments, reads source file name from os.Args
 // Read the instructions, resolve symbols, and emit the assembled code
 func main() {
+
+// 1. read source file into array
+// 2. compile instructions one at a time
+// 3. ???
+// 4. profit
+
 	f := NewSourceFile(os.Args[1])
 	a := NewAssembler()
 	a.Assemble(f)
