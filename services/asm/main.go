4e62ffcd73d4c19c0f02561858ac49d0e401cb80
John Trammell <john.trammell@gmail.com>
2024-11-02 20:25:48 -0500

    save point

--- services/asm/main.go
+++ services/asm/main.go
@@ -9,13 +9,43 @@ import (
 // Read the instructions, resolve symbols, and emit the assembled code
 func main() {
 
-// 1. read source file into array
-// 2. compile instructions one at a time
-// 3. ???
-// 4. profit
+	// read source file into `lines`
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatalf("os.Open: %s", err)
+	}
+	defer file.Close()
 
-	f := NewSourceFile(os.Args[1])
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	err = scanner.Err()
+	if err() != nil {
+		log.Fatalf("scanner.Scan: %s", err)
+	}
+
+	// 2. compile instructions one at a time
+	// 3. ???
+	// 4. profit
+
 	a := NewAssembler()
-	a.Assemble(f)
+	for _, instruction in = a.Assemble(lines) {
+
+
+	}
 	a.Emit()
+
+
+
+        out = a.SymbolTable.String()
+        for _, inst := range a.Instructions {
+                out += fmt.Sprintln(inst)
+        }
+        return
+}
+
+
+
 }
