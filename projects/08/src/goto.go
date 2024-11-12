9f5b8b966ea192914e16e26ecfe5b714f3eaab58
John Trammell <john.trammell@gmail.com>
2024-11-12 15:11:35 -0600

    add if-goto

--- projects/08/src/goto.go
+++ projects/08/src/goto.go
@@ -6,8 +6,7 @@ import (
 
 // push values from the stack to various segments
 func (cw *CodeWriter) writeGoto(label string) (error) {
-	format := `
-// goto %[1]s
+	format := `// goto %[1]s
 @%[1]s
 0; JMP`
 	_, err := fmt.Fprintf(cw.Writer, format + "\n\n", label)
