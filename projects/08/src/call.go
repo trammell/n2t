b4bc0a424118598bd01d59bc765dba43e0bd224a
John Trammell <john.trammell@gmail.com>
2024-11-12 15:27:02 -0600

    sketch in call semantics

---
+++ projects/08/src/call.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// write assembly code equivalent to a VM label command
+func (cw *CodeWriter) writeCall(fname string, nargs int) (error) {
+	format := `// call %[1]s %[2]d
+@RETURN_%[1]s
+
+push LCL 
+push ARG
+push THIS
+push THAT
+ARG = SP - n - 5 ???
+LCL = SP
+goto f
+
+(RETURN_%[1]s_)
+`
+	_, err := fmt.Fprintf(cw.Writer, format, fname, nargs)
+	return err
+}
