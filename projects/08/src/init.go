e2ae6a6d7a9c1ed85f1d12b60ff8482b97130283
John Trammell <john.trammell@gmail.com>
2024-11-12 14:57:53 -0600

    add asm to bootstrap VM

---
+++ projects/08/src/init.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+// push values from the stack to various segments
+func (cw *CodeWriter) writeInit() (error) {
+	asm := `// init: set stack pointer and call Sys.init
+@256
+D=A
+@SP
+M=D
+@Sys.init
+0; JMP`
+	_, err := fmt.Fprintf(cw.Writer, asm + "\n\n")
+	return err
+}
